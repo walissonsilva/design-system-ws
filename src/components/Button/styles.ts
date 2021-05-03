5b711710a90262e53022b3d8faa116d1fa13ff48
Walisson Silva <walissonsilva10@gmail.com>
2021-05-03 11:22:57 -0300

    first commit

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  cursor: pointer;
+  background-color: #0000dd;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: white;
+  border: 0;
+  font-size: 1rem;
+  font-weight: 600;
+`
